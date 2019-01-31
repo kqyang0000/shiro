ed8c91a44c81f91b1f1ecf243fa16d8f0211bf6d
kayang0000 <18410422493@163.com>
2019-01-31 17:23:04 +0800

    single realm test

---
+++ src/main/java/com/sl/realm/MyRealm1.java
@@ -0,0 +1,35 @@
+package com.sl.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm1 implements Realm {
+    @Override
+    public String getName() {
+        return "myrealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        // 仅支持UsernamePasswordToken
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // 得到用户名
+        String username = (String) authenticationToken.getPrincipal();
+        // 得到密码
+        String password = new String((char[]) authenticationToken.getCredentials());
+        if (!"zhang".equals(username)) {
+            // 如果用户名错误
+            throw new UnknownAccountException();
+        }
+        if (!"123".equals(password)) {
+            // 如果密码错误
+            throw new IncorrectCredentialsException();
+        }
+        // 如果身份认证成功，返回一个AuthenticationInfo实现
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
